495390b514a86e19e2115ea84f37222ca7e2e6b9
Thauan <dev.thauan.rodrigues@gmail.com>
2024-08-23 20:47:38 -0300

    sobrecarga de parâmetros

---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/dominio/Anime.java
@@ -0,0 +1,49 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio;
+
+public class Anime {
+    private String nome;
+    private String tipo;
+    private int episodios;
+    private String genero;
+
+    public void init(String nome, String tipo, int episodios){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+    }
+
+    public void init(String nome, String tipo, int episodios, String genero){
+        this.init(nome, tipo, episodios);
+        this.genero = genero;
+    }
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+    }
+
+    public void setGenero(String genero){
+        this.genero = genero;
+    }
+    public String getGenero(){
+        return this.genero;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setEpisodios(int episodios){
+        this.episodios = episodios;
+    }
+    public int getEpisodios() {
+        return this.episodios;
+    }
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+    public String getTipo(){
+        return this.tipo;
+    }
+}
