c85ff0d262d2d5bbd7c76b8297c83a092c70997d
Thauan <dev.thauan.rodrigues@gmail.com>
2024-08-23 23:48:33 -0300

    modificadores estaticos

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Carro.java
@@ -0,0 +1,44 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
+
+public class Carro {
+    private String nome;
+    private double velocidadeMaxima;
+    private static double velocidadeLimite = 250;
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public void imprime() {
+        System.out.println("-----------");
+        System.out.println("Nome " + this.nome);
+        System.out.println("Velocidade Máxima " + this.velocidadeMaxima);
+        System.out.println("Velocidade Eletronica " + Carro.velocidadeLimite);
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public void setVelocidadeLimite(double velocidadeLimite) {
+        this.velocidadeLimite = velocidadeLimite;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+}
