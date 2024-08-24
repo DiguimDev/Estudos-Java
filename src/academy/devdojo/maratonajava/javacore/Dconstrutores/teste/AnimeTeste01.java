db00d951f9274f7d4f4c8dd6f4ab7b3df1525c59
Thauan <dev.thauan.rodrigues@gmail.com>
2024-08-23 23:01:25 -0300

    Construtores e Bloco de Inicialização

---
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/teste/AnimeTeste01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.teste;
+
+import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Haikyuu", "TV",12,"Ação", "Production IG");
+        anime.imprime();
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicializaçao/domain/Anime.java
@@ -0,0 +1,34 @@
+package academy.devdojo.maratonajava.javacore.Eblocosinicializaçao.domain;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+    {
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+        for(int episodio: this.episodios){
+            System.out.print(episodio + " ");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
