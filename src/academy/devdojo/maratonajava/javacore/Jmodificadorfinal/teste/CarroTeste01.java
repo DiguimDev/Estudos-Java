e2eac8a388cede479825531857371af04d26f55a
Thauan <dev.thauan.rodrigues@gmail.com>
2024-08-28 23:08:25 -0300

    Modificador Final tipos primitivos

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/teste/CarroTeste01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.teste;
+
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+
+public class CarroTeste01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
