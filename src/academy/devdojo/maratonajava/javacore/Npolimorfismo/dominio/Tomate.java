0b55045abb73436fe91a7868344c91616c4bccda
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-04 16:02:32 -0300

    Introdução ao polimorfismo utilizando referencia de classes abstratas

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio;
 
 public class Tomate extends Produto{
     public static final double IMPOSTO_POR_CENTO = 0.06;
+    private String dataValidade;
     public Tomate(String nome, double valor) {
         super(nome, valor);
     }
@@ -10,5 +11,13 @@ public class Tomate extends Produto{
     public double calcularImposto() {
         System.out.println("Calculando imposto do tomate");
         return this.valor * Tomate.IMPOSTO_POR_CENTO;
+    }
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
     }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTeste02.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+
+public class ProdutoTeste02 {
+    public static void main(String[] args) {
+        Produto produto = new Computador("Ryzen 9",3000);
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+        System.out.println("------------------------");
+        Produto produto2 = new Tomate("Ceciliano",20);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
+        System.out.println(produto2.calcularImposto());
+    }
+}
