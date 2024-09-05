507053921027784e3e29d8daf1ba81a269b30c00
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-05 01:14:05 -0300

    Correção do nome da classe e finalização da parte intermediaria do aprendizado

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/serviço/RepositorioBancoDeDados.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/serviço/RepositorioBancoDeDados.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.javacore.Npolimorfismo.serviço;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.repositorio.Repositorio;
 
-public class RepositorioBandoDeDados implements Repositorio {
+public class RepositorioBancoDeDados implements Repositorio {
     @Override
     public void salvar() {
         System.out.println("Salvando no banco de dados");
--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/RepositorioTeste.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/RepositorioTeste.java
@@ -2,13 +2,18 @@ package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.repositorio.Repositorio;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.serviço.RepositorioArquivo;
-import academy.devdojo.maratonajava.javacore.Npolimorfismo.serviço.RepositorioBandoDeDados;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.serviço.RepositorioBancoDeDados;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.serviço.RepositorioMemoria;
 
 public class RepositorioTeste {
     public static void main(String[] args) {
         Repositorio  repositorio = new RepositorioMemoria();
         repositorio.salvar();
-
+        Repositorio repositorio1 = new RepositorioBancoDeDados();
+        System.out.println("\n------------------");
+        repositorio1.salvar();
+        Repositorio repositorio2 = new RepositorioArquivo();
+        System.out.println("---------------------");
+        repositorio2.salvar();
     }
 }
