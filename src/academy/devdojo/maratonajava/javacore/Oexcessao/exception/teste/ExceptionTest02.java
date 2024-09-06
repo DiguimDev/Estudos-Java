c1c3bcb82c972d75e1446062f5c196603a95835b
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-06 10:35:51 -0300

    Tratamento de excessões Try catch, throws e tratamento de forma hibrida

---
+++ src/academy/devdojo/maratonajava/javacore/Oexcessao/exception/teste/ExceptionTest02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Oexcessao.exception.teste;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest02 {
+    public static void main(String[] args) throws IOException{
+    criarNovoArquivo();
+    }
+    private static void criarNovoArquivo() throws IOException{
+        File file = new File("arquivo/teste.txt");
+        try{
+            boolean criado = file.createNewFile();
+            System.out.println("Arquivo criado "+ criado);
+        }catch (IOException e){
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
