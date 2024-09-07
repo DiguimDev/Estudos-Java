2a66b016b0268531049b231286c6f12c7d8ac285
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-07 15:12:04 -0300

    Wrappers valores se tornam classes e nao mais tipos primitivos

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrappers/Test/WrapperTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Pwrappers.Test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        //Regra aplicada de Bits
+        byte byteP = 1;
+        short shortP = 13;
+        int intP = 145;
+        long longP = 1500;
+        float floatP = 13.21F;
+        double doubleP = 15.12;
+        char charP = 'W';
+        boolean booleanp = true;
+
+        // Regra aplicada de Polimorfismo
+        Byte byteW = 1;
+        Short shortW = 13;
+        Integer intW = 145;
+        Long longW = 1500L;
+        Float floatW = 13.21F;
+        Double doubleW = 15.12;
+        Character charW = 'W';
+        Boolean booleanW = true;
+    }
+}
