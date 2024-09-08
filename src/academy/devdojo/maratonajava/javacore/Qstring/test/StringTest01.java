3729782fde22a677405d8698cd984a1a1d5a5443
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-08 03:41:33 -0300

    Conceito de Pool String e Strings imutáveis

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        String nome = "Thauan"; //String constant pool
+        String nome2 = "Thauan";
+        System.out.println(nome == nome2);
+    }
+}
