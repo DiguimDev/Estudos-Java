48db1db61d2102df7d35dd9f925f174139365030
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-08 12:07:28 -0300

    Performance de Strings e introdução ao stringbuilder

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringPerformanceTeste.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringPerformanceTeste {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatStrig(10000);
+        long fim = System.currentTimeMillis();
+        System.out.println("O tempo gasto foi de " + (fim - inicio));
+
+        inicio = System.currentTimeMillis();
+        concatStrigBuilder(10000);
+        fim = System.currentTimeMillis();
+        System.out.println("O tempo gasto foi de " + (fim - inicio));
+    }
+    private static void concatStrig(int tamanho){
+        String texto = "";
+        for (int i = 0; i < tamanho; i++) {
+            texto += i;
+        }
+    }
+    private static void concatStrigBuilder(int tamanho){
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+}
