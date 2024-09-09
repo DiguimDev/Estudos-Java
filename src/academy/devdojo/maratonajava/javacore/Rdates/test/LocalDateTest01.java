a4383bbaa25ac91c52d9a00cc77211de11cd0f69
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-09 02:41:16 -0300

    LocalDate

---
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/LocalDateTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+        System.out.println(date.getYear());
+        System.out.println(date.getMonth());
+        System.out.println(date.getDayOfMonth());
+        System.out.println(date.getDayOfWeek());
+        System.out.println(date.lengthOfMonth());
+        ;
+    }
+}
