710856d549cf22fa0040aafed915c86f8f4e32a7
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-08 22:14:36 -0300

    Formatação de números

--- src/academy/devdojo/maratonajava/javacore/Sformatação/test/LocalTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Sformatação/test/LocalTest02.java
@@ -5,5 +5,16 @@ import java.util.Locale;
 public class LocalTest02 {
     public static void main(String[] args) {
         System.out.println(Locale.getDefault());
+        String[] isoCountries = Locale.getISOCountries();
+        String[] isoLanguages = Locale.getISOLanguages();
+        for (String isoLanguage : isoLanguages) {
+            System.out.print(isoLanguage+ " ");
+        }
+        System.out.println("\n------------------------------------");
+        for (String isoCountry : isoCountries) {
+            System.out.print(isoCountry+ " ");
+        }
+
+
     }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Sformatação/test/NumberFormatTest01.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.Sformatação.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociaçãoexercise.dominio.Local;
+
+import java.text.NumberFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class NumberFormatTest01 {
+    public static void main(String[] args) {
+        Locale localePt = new Locale("pt","BR");
+        Locale localeIt = Locale.ITALY;
+        Locale localeJapan = Locale.JAPAN;
+        Locale localeEua = Locale.US;
+        NumberFormat[] nfa = new NumberFormat[4];
+        nfa[0] = NumberFormat.getInstance();
+        nfa[1] = NumberFormat.getInstance(localeJapan);
+        nfa[2] = NumberFormat.getInstance(localeIt);
+        nfa[3] = NumberFormat.getInstance(localeEua);
+
+        double valor = 10000.2130;
+
+        for (NumberFormat numberFormat : nfa) {
+            System.out.println(numberFormat.format(valor));
+        }
+        
+    }
+}
