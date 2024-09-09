1a3d870ad56fb9e87fabde6ac56c177987ebece4
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-09 11:36:01 -0300

    Classe de tempo Instant

---
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/InstantTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class InstantTest01 {
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println(now);
+        System.out.println(LocalDateTime.now());
+        System.out.println(now.getNano());
+        System.out.println(now.getEpochSecond());
+    }
+}
