5f9bf85a3e6ad3d8b19396c5ee0ba7865512876c
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-09 00:23:58 -0300

    SimpleDateFormat

---
+++ src/academy/devdojo/maratonajava/javacore/Sformatação/test/SimpleDateFormatTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Sformatação.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatTest01 {
+    public static void main(String[] args) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        System.out.println(simpleDateFormat.format(new Date()));
+    }
+}
