44b16ceb08295e1de5d7ba9a5ddc3c88ac046799
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-09 00:11:55 -0300

    Formatação de Dinheiro

--- src/academy/devdojo/maratonajava/introduçao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introduçao/Aula02TiposPrimitivos.java
@@ -4,7 +4,7 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         int age = 10;
         long numeroGrande = 1000000;
-        double salarioDouble = 2000;
+        double salarioDouble = 2000.67;
         float salarioFloat = 2500.0F;
         byte idadeByte = 10;
         short idadeShort = 10;
