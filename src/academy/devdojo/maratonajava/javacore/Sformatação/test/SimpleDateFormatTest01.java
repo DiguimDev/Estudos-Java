1defccae18ac3472abc015cea5e45ef753981d5a
Thauan <thauan@arch.localdomain>
2024-09-11 13:58:30 -0300

    Testando github no archlinux

--- src/academy/devdojo/maratonajava/javacore/Sformatação/test/SimpleDateFormatTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Sformatação/test/SimpleDateFormatTest01.java
@@ -7,7 +7,7 @@ public class SimpleDateFormatTest01 {
     public static void main(String[] args) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
         System.out.println(simpleDateFormat.format(new Date()));
-        System.out.println("teste");
+        System.out.println("teste1");
     }
 }
     
