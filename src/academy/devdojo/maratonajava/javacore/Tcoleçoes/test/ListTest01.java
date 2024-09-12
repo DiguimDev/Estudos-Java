e0efb2a3318bb50b9063d689134d981a0d2da9a9
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-11 21:31:21 -0300

    Collections, alguns principios

--- src/academy/devdojo/maratonajava/javacore/Tcoleçoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Tcoleçoes/test/ListTest01.java
@@ -6,16 +6,29 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>();
+        List<String> nomes2 = new ArrayList<>();
         nomes.add("Thauan");
         nomes.add("Thauan Rodrigues");
         nomes.add("Javeiro");
 
-        for (Object nome: nomes){
+        nomes2.addAll(nomes);
+
+        nomes.remove("Thauan");
+
+        for (Object nome: nomes2){
             System.out.println(nome);
         }
         System.out.println("-------------------");
         for (int i = 0; i < nomes.size(); i++) {
             System.out.println(nomes.get(i));
+        }
+
+        List<Integer> listaNumerada = new ArrayList<>();
+        for (int i = 0; i < 11; i++) {
+            listaNumerada.add(i);
+        }
+        for (Integer listaNumerica: listaNumerada){
+            System.out.print(listaNumerica + " ");
         }
     }
 }
