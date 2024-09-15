3f3c70cff6f15a329872cda29b8244c023aa9364
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-15 15:40:10 -0300

    NavigableSet e funções do mesmo

--- src/academy/devdojo/maratonajava/javacore/Tcoleçoes/test/NavigableSetTest0.java
+++ src/academy/devdojo/maratonajava/javacore/Tcoleçoes/test/NavigableSetTest0.java
@@ -12,13 +12,20 @@ class SmartphoneMarcaComparator implements Comparator<Smartphone>{
         return o1.getMarca().compareTo(o2.getMarca());
     }
 }
+class MangaPriceComparator implements Comparator<Manga>{
+
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return Double.compare(o1.getPreco(), o2.getPreco());
+    }
+}
 public class NavigableSetTest0 {
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
         Smartphone smartphone = new Smartphone("Tijolo", "40028922");
         set.add(smartphone);
         System.out.println(set);
-        NavigableSet<Manga> mangas = new TreeSet<>(); // Tree Set origaniza automaticamente através do CompareTo
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPriceComparator()); // Tree Set origaniza automaticamente através do CompareTo
         mangas.add(new Manga(5L,"Haikyuu",18.99, 0));
         mangas.add(new Manga( 3L,"One Piece",19.99, 6));
         mangas.add(new Manga(1L,"Dragon Ball",6.82, 2));
@@ -28,5 +35,19 @@ public class NavigableSetTest0 {
             System.out.println(manga);
         }
 
+        Manga naruto = new Manga(31L, "Naruto", 6.83, 6);
+        // lower <
+        // floor <=
+        // higher >
+        // ceiling >=
+        System.out.println(mangas.lower(naruto));
+        System.out.println(mangas.floor(naruto));
+        System.out.println(mangas.higher(naruto));
+        System.out.println(mangas.ceiling(naruto));
+
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollFirst());
+        System.out.println(mangas.pollLast());
+        System.out.println(mangas.size());
     }
 }
