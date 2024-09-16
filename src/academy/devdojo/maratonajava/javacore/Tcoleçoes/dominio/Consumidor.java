de9aae626fde1297281cdb7796980ef2a724f3b6
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-15 21:10:38 -0300

    Map com Classes

--- src/academy/devdojo/maratonajava/javacore/Tcoleçoes/dominio/Consumidor.java
+++ src/academy/devdojo/maratonajava/javacore/Tcoleçoes/dominio/Consumidor.java
@@ -8,7 +8,7 @@ public class Consumidor {
     private String nome;
 
     public Consumidor(String nome) {
-        this.id = ThreadLocalRandom.current().nextLong();
+        this.id = ThreadLocalRandom.current().nextLong(0,100_000);
         this.nome = nome;
     }
 
@@ -31,5 +31,21 @@ public class Consumidor {
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Tcoleçoes/test/MapTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Tcoleçoes/test/MapTest02.java
@@ -1,10 +1,26 @@
 package academy.devdojo.maratonajava.javacore.Tcoleçoes.test;
 
 import academy.devdojo.maratonajava.javacore.Tcoleçoes.dominio.Consumidor;
+import academy.devdojo.maratonajava.javacore.Tcoleçoes.dominio.Manga;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapTest02 {
     public static void main(String[] args) {
         Consumidor consumidor1 = new Consumidor("Geraldo");
         Consumidor consumidor2 = new Consumidor("Itachi");
+        Manga manga1 = new Manga(5L, "Haikyuu", 18.99);
+        Manga manga2 = new Manga(3L, "One Piece", 19.99);
+        Manga manga3 = new Manga(1L, "Dragon Ball", 6.82);
+        Manga manga4 = new Manga(2L, "Berserk", 13.65);
+        Manga manga5 = new Manga(4L, "Pokemon", 12.12);
+
+        Map<Consumidor, Manga> map = new HashMap<>();
+        map.put(consumidor1, manga2);
+        map.put(consumidor2, manga5);
+        for (Map.Entry<Consumidor, Manga> consumidorMangaEntry : map.entrySet()) {
+            System.out.println(consumidorMangaEntry.getKey().getNome() + " -> " + consumidorMangaEntry.getValue().getNome());
+        }
     }
 }
