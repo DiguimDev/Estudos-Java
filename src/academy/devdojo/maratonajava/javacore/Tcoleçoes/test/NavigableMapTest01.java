f3fc2174bd38a649d546d81786a28cf5d1d45346
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-17 00:37:26 -0300

    NavigableMap

---
+++ src/academy/devdojo/maratonajava/javacore/Tcoleçoes/test/NavigableMapTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Tcoleçoes.test;
+
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class NavigableMapTest01 {
+    public static void main(String[] args) {
+        NavigableMap<String, String> map = new TreeMap<>();
+        map.put("A", "Letra A");
+        map.put("B", "Letra B");
+        map.put("C", "Letra C");
+        map.put("D", "Letra D");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+        System.out.println(map.headMap("C"));
+    }
+}
