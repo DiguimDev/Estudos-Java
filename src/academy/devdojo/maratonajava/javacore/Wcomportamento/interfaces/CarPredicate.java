07932db4c3edd54bf9de116660638a0e64d51918
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-21 00:48:11 -0300

    Lambdas e introdução a programação funcional

--- src/academy/devdojo/maratonajava/javacore/Wcomportamento/interfaces/CarPredicate.java
+++ src/academy/devdojo/maratonajava/javacore/Wcomportamento/interfaces/CarPredicate.java
@@ -3,5 +3,9 @@ package academy.devdojo.maratonajava.javacore.Wcomportamento.interfaces;
 import academy.devdojo.maratonajava.javacore.Wcomportamento.dominio.Car;
 
 public interface CarPredicate {
-    boolean test(Car car);
+    // anonimas, funções, concisa;
+    boolean test(Car car); // esse é o contrato da lambda;
+    // (parametro) -> <expressão>
+    // (Car car) -> car.getColor().equals("Green");
+    // para utilizar lambdas precisa de uma interface Funcional, na qual so tem uma interface sem corpo;
 }
