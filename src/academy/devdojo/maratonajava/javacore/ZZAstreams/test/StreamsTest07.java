7df00994a6102347c9aa4e6cc03a044d8808cc57
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-28 11:30:03 -0300

    Reduce pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/ZZAstreams/test/StreamsTest07.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.ZZAstreams.test;
+
+import java.util.List;
+
+public class StreamsTest07 {
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+        integers.stream().reduce(Integer::sum).ifPresent(System.out::println);
+        integers.stream().reduce((x,y) -> x+y).ifPresent(System.out::println);
+
+        System.out.println(integers.stream().reduce(0, Integer::sum));
+        System.out.println(integers.stream().reduce(0,(x,y) -> x+y));
+
+        System.out.println(integers.stream().reduce(1, (x,y) -> x*y));
+        integers.stream().reduce((x,y) -> x*y).ifPresent(System.out::println);
+
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/ZZAstreams/test/StreamsTest08.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonajava.javacore.ZZAstreams.test;
+
+public class StreamsTest08 {
+}
