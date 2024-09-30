c1b2007abd03c743271d3a7c555b395ac03e07a6
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-30 11:19:51 -0300

    Adicinando Lombok e Log4J2 no projeto

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZCjdbc/repository/ProducerRepository.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZCjdbc/repository/ProducerRepository.java
@@ -2,18 +2,20 @@ package academy.devdojo.maratonajava.javacore.ZZCjdbc.repository;
 
 import academy.devdojo.maratonajava.javacore.ZZCjdbc.conn.ConectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZCjdbc.dominio.Producer;
+import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+@Log4j2
 public class ProducerRepository {
     public static void save(Producer producer){
         String sql = "INSERT INTO `anime_store`. `producer` (`name`) VALUES ('%s');".formatted(producer.getName());
         try(Connection conn = ConectionFactory.getConnection();
             Statement stmt = conn.createStatement()){
             int rowsAffected = stmt.executeUpdate(sql);
-            System.out.println(rowsAffected);
+            log.info("Database rows affected {}",rowsAffected);
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/java/academy/devdojo/maratonajava/javacore/ZZCjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZCjdbc/test/ConnectionFactoryTest01.java
@@ -2,12 +2,19 @@ package academy.devdojo.maratonajava.javacore.ZZCjdbc.test;
 
 import academy.devdojo.maratonajava.javacore.ZZCjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZCjdbc.repository.ProducerRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ConnectionFactoryTest01 {
+    private static final Logger log = LogManager.getLogger(ConnectionFactoryTest01.class);
+
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder
-                .builder()
-                .name("NHK").build();
-        ProducerRepository.save(producer);
+//        Producer producer = Producer.builder().name("Studio Deen").build();
+//        ProducerRepository.save(producer);
+        log.debug("INFO");
+        log.debug("INFO");
+        log.warn("INFO");
+        log.error("INFO");
+        log.trace("INFO");
     }
 }
